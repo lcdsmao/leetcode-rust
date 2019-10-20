7987427b0a649df995c71bfd718bcbd440f6f117
Mao <lcdsmao@gmail.com>
2019-10-20 12:21:37 +0900

    Add 030

---
+++ p030_substring_with_concatenation_of_all_words/src/lib.rs
@@ -0,0 +1,35 @@
+pub struct Solution {}
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn find_substring(s: String, words: Vec<String>) -> Vec<i32> {
+        let word_len = if let Some(l) = words.first() {
+            l.len()
+        } else {
+            return vec![];
+        };
+        let all_len = word_len * words.len();
+        let word_map = Self::convert_to_hash_map(words);
+        s.as_bytes().windows(all_len)
+            .map(|w| {
+                let ws: Vec<String> = w.chunks(word_len)
+                    .map(|s| String::from_utf8_lossy(s).into())
+                    .collect();
+                Self::convert_to_hash_map(ws)
+            })
+            .enumerate()
+            .filter_map(|(i, m)| {
+                if m == word_map { Some(i as i32) } else { None }
+            })
+            .collect()
+    }
+
+    fn convert_to_hash_map(strs: Vec<String>) -> HashMap<String, usize> {
+        let mut map = HashMap::new();
+        for s in strs {
+            *map.entry(s).or_insert(0) += 1;
+        }
+        map
+    }
+}
---
+++ p030_substring_with_concatenation_of_all_words/src/main.rs
@@ -0,0 +1,11 @@
+use p030_substring_with_concatenation_of_all_words::Solution;
+
+fn main() {
+    let res = Solution::find_substring(
+        "barfoothefoobarman".to_string(),
+        vec![
+            "foo".to_string(),
+            "bar".to_string(),
+        ]);
+    assert_eq!(res, vec![0, 9]);
+}
