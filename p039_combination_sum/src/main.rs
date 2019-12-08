e1928f8499d8b29c6eebc7a2b3e194e0c04ce20d
Mao <lcdsmao@gmail.com>
2019-12-08 14:55:48 +0900

    Add 039

---
+++ p039_combination_sum/src/main.rs
@@ -0,0 +1,10 @@
+use p039_combination_sum::Solution;
+
+fn main() {
+    let candidates = vec![2, 3, 6, 7];
+    let ans = vec![
+        vec![2, 2, 3],
+        vec![7],
+    ];
+    assert_eq!(Solution::combination_sum(candidates, 7), ans);
+}
