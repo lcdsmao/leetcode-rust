df7d977aa53abb0c59c0cb9755aaea478ccbb7af
Mao <lcdsmao@gmail.com>
2019-12-29 11:12:37 +0900

    Add 055

---
+++ problem/p055_jump_game/src/lib.rs
@@ -0,0 +1,25 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn can_jump(nums: Vec<i32>) -> bool {
+        let mut left_most = nums.len() - 1;
+        for i in (0..nums.len() - 1).rev() {
+            if i + nums[i] as usize >= left_most {
+                left_most = i;
+            }
+        }
+        left_most == 0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(Solution::can_jump(vec![2, 3, 1, 1, 4]), true);
+        assert_eq!(Solution::can_jump(vec![3, 2, 1, 0, 4]), false);
+    }
+}
